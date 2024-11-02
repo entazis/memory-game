a6a4fe4eb11e3b196c13c23b666537934d14542c
beam-bence <bence@beam.live>
2024-11-02 15:04:36 +0100

    refactor(game): render Header and Cards, remove example

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,9 @@
-import React from 'react';
-import {render, screen} from '@testing-library/react';
-import {Provider} from 'react-redux';
-import {store} from './app/store';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
+test("renders learn react link", () => {
+  render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
