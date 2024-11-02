ce4a450bf9652b4c2a9155e76638726f0b9e096c
beam-bence <bence@beam.live>
2024-11-02 14:47:13 +0100

    feat(card): card flip animation

--- src/game/Cards.tsx
+++ src/game/Cards.tsx
@@ -1,16 +1,18 @@
-import { Col, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import Card from "./card/Card";
 
 //TODO render 8 cards on large screen
 function Cards() {
   return (
-    <Row>
-      {Array.from({ length: 3 }).map((_, idx) => (
-        <Col xs={4} sm={3} md={2} key={idx}>
-          <Card />
-        </Col>
-      ))}
-    </Row>
+    <Container>
+      <Row>
+        {Array.from({ length: 3 }).map((_, idx) => (
+          <Col xs={4} sm={3} md={2} key={idx}>
+            <Card />
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 }
 
--- src/game/card/Card.tsx
+++ src/game/card/Card.tsx
@@ -1,22 +1,49 @@
+import { useCallback, useState } from "react";
 import BootstrapCard from "react-bootstrap/Card";
 import fox from "../../assets/fox.png";
+import dog from "../../assets/dog.png";
+import "./Card.css";
 
 function Card() {
+  const [isFlipped, setFlipped] = useState(false);
+
+  const flipCard = useCallback(() => {
+    setFlipped((prev) => !prev);
+  }, []);
+
   return (
     <BootstrapCard
+      className={`flip-card ${isFlipped ? "flipped" : ""}`}
       style={{
+        //TODO move styles to a CSS module file, use className
         width: "100px",
         height: "150px",
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
       }}
+      onClick={flipCard}
     >
-      <BootstrapCard.Img
-        variant="top"
-        src={fox}
-        style={{ maxWidth: "100%", maxHeight: "100%" }}
-      />
+      <div className="flip-card-inner">
+        <div className="flip-card-front">
+          <div className="card-content">
+            <BootstrapCard.Img
+              variant="top"
+              src={fox}
+              className="bootstrap-card"
+            />
+          </div>
+        </div>
+        <div className="flip-card-back">
+          <div className="card-content">
+            <BootstrapCard.Img
+              variant="top"
+              src={dog}
+              className="bootstrap-card"
+            />
+          </div>
+        </div>
+      </div>
     </BootstrapCard>
   );
 }
